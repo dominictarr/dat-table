d7b565c5a903496f16f884e9fcec9ff895640d00
Dominic Tarr <dominic.tarr@gmail.com>
2018-11-06 22:32:57 +0530

    help output, so I remember how it works next time I use it

--- bin.js
+++ bin.js
@@ -10,17 +10,36 @@ var join = opts.join || opts.j
 var filter = opts.columns || opts.c
 var slice = opts.slice || opts.s
 
+var columns_help = '  --columns COLS\n' +
+  '    # COLS must be comma separated column numbers to keep. eg, 0,1,4'
+var slice_help = 
+  '  --slice START,LENGTH\n' +
+  '    # slice rows, from start to start+length'
+var join_help = 
+  '  [files...] --join {names,...}?\n' +
+  '    # join multiple files into one table by the first column\n' +
+  '    # if names are provided, they will used in output, instead of filenames'
+
 if(filter) {
   if('string' !== typeof filter)
-    error('--columns COLS\n'
-        + '# COLS must be comma separated column numbers to keep. 0,1,4')
+    error(columns_help)
   filter = filter.split(/,\s*/).map(Number)
 }
 
 if(slice) {
   if('string' !== typeof slice)
-    error('--slice START,LENGTH')
+    error()
   slice = slice.split(/,\s*/).map(Number)
+}
+
+if(opts.help || (process.stdin.isTTY && opts._.length === 0)) {
+  console.error('dat-table [files...] --{join,columns,slice,json,help}')
+  console.error(join_help)
+  console.error(columns_help)
+  console.error(slice_help)
+  console.error('  --json\n    #output as json format')
+  console.error('  --help\n    #display this help message')
+  process.exit(0)
 }
 
 function error (message) {
@@ -36,7 +55,6 @@ function dump(table) {
 }
 
 function mutate (table) {
-  console.error(slice, filter)
   if(slice)  table = table.slice.apply(table, slice)
   if(filter) table = table.filterColumns(filter)
   return table
@@ -53,10 +71,10 @@ if(!process.stdin.isTTY) {
 } else {
   var fs = require('fs')
 
-  var table = Table.join(opts._.map(function (filename) {
+  var names = opts.join != true ? join.split(',').map(function (s) { return s.trim() }) : []
+  var table = Table.join(opts._.map(function (filename, i) {
     var data = fs.readFileSync(filename, 'utf8')
-    var name = path.basename(filename).replace(/\.\w+$/,'')
-    return mutate(Table.createTable(data, {name: name}))
+    return mutate(Table.createTable(data, {name: names[i] || path.basename(filename)}))
   }))
 
   dump(table)
